455af68109a279bebe31f25162e97ed03d207b45
alifrahmadian <50415100+alifrahmadian@users.noreply.github.com>
2021-06-30 14:01:15 +0700

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\MedicineController;
+use App\Http\Controllers\MedicineStockController;
+
+
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +26,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Member Data API for Dashboard
+Route::get('/dashboard/member_data', [RegisterController::class, 'show'])->name('show');
+Route::get('/dashboard/member_data/edit_member/{user_id}', [RegisterController::class, 'edit']);
+Route::put('/dashboard/member_data/edit_member/{user_id}', [RegisterController::class, 'update']);
+Route::delete('/dashboard/member_data/{user_id}', [RegisterController::class, 'destroy']);
+
+
+// Patient Data API for Dashboard
+Route::get('dashboard/patient_data', [PatientController::class, 'showAll']);
+
+// Medicines API
+Route::get('dashboard/vaccine_data', [MedicineController::class, 'index']);
+Route::get('dashboard/vaccine_data/edit_vaccine/{medicine_id}', [MedicineController::class, 'edit']);
+Route::put('dashboard/vaccine_data/edit_vaccine/{medicine_id}', [MedicineController::class, 'update']);
+Route::delete('dashboard/vaccine_data/{medicine_id}', [MedicineController::class, 'destroy']);
+Route::post('dashboard/vaccine_data/add_vaccine', [MedicineController::class, 'store']);
+
+// Medicine Stocks API
+Route::get('dashboard/vaccine_data/edit_vaccine/{medicine_id}/add_vaccine_stock', [MedicineStockController::class, "create"]);
+Route::post('dashboard/vaccine_data/edit_vaccine/{medicine_id}/add_vaccine_stock', [MedicineStockController::class, "store"]);
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\MedicineController;
+
+
 
 
 
@@ -26,6 +30,16 @@ Route::get('/register', function () {
     return view('auth.register');
 });
 
+
+
+Route::get('/member/{user_id}/patient_data', function () {
+    return view('member.patient_data');
+});
+
+Route::get('/member/{user_id}/patient_data/add_patient', function () {
+    return view('member.add_patient');
+});
+
 // Route::get('/login', function () {
 //     return view('auth.login');
 // });
@@ -38,11 +52,54 @@ Route::get('/dashboard', function () {
     return view('dashboard.dashboard');
 });
 
+Route::get('/dashboard/member_data', function () {
+    return view('dashboard.dashboardPages.member_data');
+});
+
+Route::get('/dashboard/member_data/edit_member/{user_id}', function () {
+    return view('dashboard.dashboardPages.memberData.edit_member');
+});
+
+Route::get('/dashboard/vaccine_data/edit_vaccine/{user_id}', function () {
+    return view('dashboard.dashboardPages.vaccineData.edit_vaccine');
+});
+
+Route::get('/dashboard/vaccine_data/edit_vaccine/{user_id}/add_vaccine_stock', function () {
+    return view('dashboard.dashboardPages.vaccineData.vaccineStockData.add_vaccine_stock');
+});
+
+
+Route::get('/dashboard/patient_data', function () {
+    return view('dashboard.dashboardPages.patient_data');
+});
+
+Route::get('/dashboard/vaccine_data', function () {
+    return view('dashboard.dashboardPages.vaccine_data');
+});
+
+Route::get('/dashboard/vaccine_data/add_vaccine', [MedicineController::class, 'create']);
+
+
+
+
+
+
 Auth::routes();
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('showLoginForm');
 route::post('/login',[LoginController::class,'login'])->name('login');
 Route::post('/',[RegisterController::class,'register'])->name('register');
+
+// Patient data route
+Route::get('/member/{user_id}/patient_data/add_patient', [PatientController::class, 'create']);
+Route::get('/member/{user_id}/patient_data', [PatientController::class, 'show']);
+Route::post('/member/{user_id}/patient_data', [PatientController::class, 'store'])->name('store');
+
+// Queue Route (Appointment)
+Route::get('member/{user_id}/appointment', [QueueController::class, 'create']);
+
+
+
 
 
 
