028819df3d8f32a15609b35ddab5ee0aa956636e
alifrahmadian <50415100+alifrahmadian@users.noreply.github.com>
2021-06-30 14:00:50 +0700

    Add files via upload

--- database/migrations/2021_06_12_050246_create_patients_table.php
+++ database/migrations/2021_06_12_050246_create_patients_table.php
@@ -14,7 +14,18 @@ class CreatePatientsTable extends Migration
     public function up()
     {
         Schema::create('patients', function (Blueprint $table) {
-            $table->id();
+            $table->increments('patient_id');
+            $table->unsignedInteger('user_id');
+            $table->string('full_name');
+            $table->string('nickname');
+            $table->string('gender');
+            $table->date('date_of_birth');
+            $table->integer('gestation');
+            $table->integer('birth_weight');
+            $table->string('food_allergy')->nullable();
+            $table->string('drug_allergy')->nullable();
+            $table->string('other_allergy')->nullable();
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2021_06_25_172525_create_medicine_stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicineStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicine_stocks', function (Blueprint $table) {
+            $table->increments('medicine_stock_id');
+            $table->unsignedInteger('medicine_id');
+            $table->integer('stock_amount');
+            $table->date('expiry_date');
+            $table->foreign('medicine_id')->references('medicine_id')->on('medicines')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicine_stocks');
+    }
+}
